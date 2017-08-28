d262554b82022799dac872a6a1f3bc9e4f467041
huangdaxia <527548875@qq.com>
2017-08-28 23:57:16 +0800

    Demo support

--- KSYProgressView/KSYProgressView/ViewController.swift
+++ KSYProgressView/KSYProgressView/ViewController.swift
@@ -9,10 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let circle = KSYProgressView()
+    let line = KSYProgressView(progressBarType: .line)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        circle.progressColor = UIColor.yellow
+        line.progressColor = UIColor.orange
+        
+        circle.frame = CGRect(x: view.center.x - 100, y: 128, width: 200, height: 200)
+        line.frame = CGRect(x: view.center.x - 140, y: 360, width: 280, height: 20)
+        
+        view.addSubview(circle)
+        view.addSubview(line)
+        
+        
+        let btn = UIButton(type: .plain)
+        btn.backgroundColor = .green
+        btn.frame = CGRect(x: view.center.x - 64, y: 420, width: 128, height: 44)
+        btn.setTitle("change progress", for: .normal)
+        btn.addTarget(self, action: #selector(changeProgress), for: .touchUpInside)
+        view.addSubview(btn)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +40,15 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @objc func changeProgress() {
+        let progress = CGFloat(Float(arc4random() % 100) / 100.0)
+        
+        let r = CGFloat(Float(arc4random() % 100) / 100.0)
+        let g = CGFloat(Float(arc4random() % 100) / 100.0)
+        let b = CGFloat(Float(arc4random() % 100) / 100.0)
+        circle.update(progress: progress, color: UIColor.init(red: r, green: g, blue: b, alpha: 1), animated: true)
+        line.update(progress: progress, color: UIColor.init(red: r, green: g, blue: b, alpha: 1), animated: true)
+    }
 
 }
 
